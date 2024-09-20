95bd4866f3e4519c778ceb603fece2564dfbd376
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-20 16:33:48 +0300

    add list and details views

--- actors_agency/views.py
+++ actors_agency/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.views import generic
-from .models import Actor, Agency, Character, ActorAgency
+from .models import Actor, Agency, Character
 
 def index(request):
     num_actors = Actor.objects.count()
@@ -19,6 +20,54 @@ def index(request):
 
     return render(request, "actors_agency/index.html", context=context)
 
-class ActorDetailView(generic.DetailView):
+
+class ActorsListView(generic.ListView):
     model = Actor
-    template_name = "actors_agency/actor_detail.html"
+    template_name = "actors_agency/actors_list.html"
+    context_object_name = "actors_list"
+    paginate_by = 4
+
+    def get_queryset(self):
+        return Actor.objects.all().order_by("username")
+
+def actors_detail_view(request:HttpRequest, pk: int):
+    actor = Actor.objects.get(id=pk)
+    context = {
+        "actor": actor,
+    }
+    return render(request, "actors_agency/actors_detail.html", context)
+
+
+class CharactersListView(generic.ListView):
+    model = Character
+    template_name = "actors_agency/characters_list.html"
+    context_object_name = "characters_list"
+    paginate_by = 4
+
+    def get_queryset(self):
+        return Character.objects.all().order_by("name")
+
+
+def character_detail_view(request: HttpRequest, pk: int):
+    character = Character.objects.get(id=pk)
+    context = {
+        "character": character,
+    }
+    return render(request, "actors_agency/character_detail.html", context)
+
+
+class AgencyListView(generic.ListView):
+    model = Agency
+    template_name = "actors_agency/agency_list.html"
+    context_object_name = "agency_list"
+    paginate_by = 4
+
+    def get_queryset(self):
+        return Agency.objects.all().order_by("name")
+
+def agency_detail_view(request: HttpRequest, pk: int):
+    agency = Agency.objects.get(id=pk)
+    context = {
+        "agency": agency,
+    }
+    return render(request, "actors_agency/agency_detail.html", context)
