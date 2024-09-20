eca432669c825a2d9117f569327fcac35c0f96bd
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-20 16:33:59 +0300

    add list and details urls

--- actors_agency/urls.py
+++ actors_agency/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import index, ActorDetailView
+from .views import (
+    index,
+    actors_detail_view,
+    ActorsListView,
+    CharactersListView,
+    AgencyListView, agency_detail_view, character_detail_view
+)
 
 urlpatterns = [
-    path("", index, name="index"),  # Головна сторінка
-    path("actor/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),  # Деталі актора
+    path('', index, name='index'),
+    path('actors/', ActorsListView.as_view(), name='actors-list'),
+    path('actors/<int:pk>/',actors_detail_view, name='actor-detail'),
+    path('agencies/',AgencyListView.as_view(), name='agency-list'),
+    path('agencies/<int:pk>/',agency_detail_view, name='agency-detail'),
+    path('characters/',CharactersListView.as_view(), name='characters-list'),
+    path('characters/<int:pk>/', character_detail_view, name='character-detail'),
 ]
 
 app_name = "actors_agency"
