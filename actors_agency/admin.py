8d236954c1e98301f534f916c7cc7641125f11ed
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-23 12:23:32 +0300

    add admin

--- actors_agency/admin.py
+++ actors_agency/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Actor, Agency, Character, ActorAgency
 
-# Register your models here.
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "gender")
+    search_fields = ("username", "first_name", "last_name")
+
+
+@admin.register(Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    list_display = ("name", "city")
+    search_fields = ("name", "city")
+
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ("name", "gender", "agency")
+    search_fields = ("name",)
+
+
+@admin.register(ActorAgency)
+class ActorAgencyAdmin(admin.ModelAdmin):
+    list_display = ('actor', 'character', 'agency', 'is_booked')
+    list_filter = ('is_booked',)
