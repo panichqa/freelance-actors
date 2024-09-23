0fd5b05b9a404b27f8867cb06628b240a37eba7c
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-23 23:04:51 +0300

    added build.sh

--- freelance_actors/settings.py
+++ freelance_actors/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://freelance-actors-yg0v.onrender.com"]
 
 
 
